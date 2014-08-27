b89b2245dd30696aab3c24abefa5ff0fb6760b57
Adrian Cunanan <adrian@adriancunanan.com>
2014-08-26 20:49:51 -0400

    Add name attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826192832) do
+ActiveRecord::Schema.define(version: 20140827004743) do
 
   create_table "todos", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140826192832) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
